dcb15186415114ad08221a2aa92d4dc9a43d60ae
npradhan-0 <npradhan@oswego.edu>
2025-04-19 14:51:26 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🧘‍♀️ Digital Detox</h1>
+      <p>Welcome! Let’s help you find your focus.</p>
+      <button>Start Detox</button>
+    </div>
+  );
+}
+
+export default App;
